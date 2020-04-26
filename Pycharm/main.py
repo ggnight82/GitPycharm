ff601bc389b2e50dfe7ee711745b14623aaff9bd
ggnight82 <hannibal0802@naver.com>
2020-04-26 22:39:08 +0900

    Hello World 5 in Master Branch

--- Pycharm/main.py
+++ Pycharm/main.py
@@ -9,5 +9,8 @@ print("hello World 2")
 #adding new item in master branch
 print("Hello World 3")
 
-#adding new tiem in Hotfix
-print("hellow World 4 ")
+#adding new item in Hotfix
+print("hello World 4 ")
+
+#adding new item in master branch
+print("Hello World 5 ")
